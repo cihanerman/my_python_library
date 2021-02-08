56a2adb1690e057a34a34e183edcfda8121697b3
Cihan ERMAN <33163384+cihanerman@users.noreply.github.com>
2021-02-08 11:57:55 +0300

    Update tools.py
    add new funtions

--- tools.py
+++ tools.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+# librays
+import random, string, datetime
+
+# class and functions
+
 class Switcher(object):
     """Python switch case
     
@@ -20,3 +26,28 @@ class Switcher(object):
 def safe_division(n, d):
     """Python safe division for ZeroDivisionError"""
     return n / d if d else 0
+
+def converts_turkish_characters_to_english_characters(word: str) -> str:
+    """ The function that converts Turkish characters to English characters
+        
+        Parameters:
+            word: Related word
+    """
+    turkish_caracters = {
+        ord(c): ord(t)
+        for c, t in zip(u"şçıöüğŞÇİÜÖĞ", u"sciougSCIUOG")
+    }
+    return word.translate(turkish_caracters)
+
+def random_string(length: int) -> str:
+    """ Ramdom string creation function
+
+        Parameters:
+            length: Number of characters
+    """
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
+
+def diff_month(d1: datetime.datetime, d2: datetime.datetime) -> int:
+    """ Calculates the difference in months between two dates """
+    return abs((d1.year - d2.year) * 12 + d1.month - d2.month)
