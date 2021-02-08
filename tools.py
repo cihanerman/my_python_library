b9516240ce8e577c6702a9471c36901a750664ae
Cihan ERMAN <33163384+cihanerman@users.noreply.github.com>
2021-02-08 11:33:28 +0300

    Update tools.py
    Switcher v2

--- tools.py
+++ tools.py
@@ -7,9 +7,12 @@ class Switcher(object):
             switch = Switcher(keys, values)
             print(switch.get_case("a")) #=> 1
             print(switch.get_case("q")) #=> None
+            switch = Switcher(keys, values, 'Not found')
+            print(switch.get_case("q")) #=> Not found
     """
-    def __init__(self, keys, values):
+    def __init__(self, keys, values, default= None):
         self.switch = dict(zip(keys, values))
+        self.default = default
     
     def get_case(self, key):
-        return self.switch.get(key, None)
+        return self.switch.get(key, self.default)
