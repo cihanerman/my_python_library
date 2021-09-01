3b59fbf5c738419d12f73d9ed03438fe9ff35fe5
Cihan ERMAN <33163384+cihanerman@users.noreply.github.com>
2021-09-01 17:48:18 +0300

    Update tools.py

--- tools.py
+++ tools.py
@@ -116,3 +116,8 @@ def chunks(lst, n):
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
+
+"""add it to the corresponding function, it will suppress the name of the calling place
+    import inspect
+    print(inspect.stack()[1][3])
+"""
